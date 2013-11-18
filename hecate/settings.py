3100231b871cbd58e00ad4dfad48ecf31ffb8d54
Nick Stenning <nick@whiteink.com>
2013-11-18 20:37:04 +0100

    settings: read X-Forwarded-Proto header
    ... and switch on a couple of CSRF cookie protection options

--- hecate/settings.py
+++ hecate/settings.py
@@ -179,6 +179,9 @@ if env.get('DJANGO_SECURE') == 'true':
     SECURE_BROWSER_XSS_FILTER = True
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https") # Heroku sends this
+    CSRF_COOKIE_SECURE = True
+    CSRF_COOKIE_HTTPONLY = True
 else:
     SECURE_SSL_REDIRECT = False
 
