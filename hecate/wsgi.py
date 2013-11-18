787f7246b1c14e4795f56662702f0f4a25b91d85
Nick Stenning <nick@whiteink.com>
2013-11-18 20:53:30 +0100

    settings: configure static file serving for Heroku

--- hecate/wsgi.py
+++ hecate/wsgi.py
@@ -27,6 +27,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hecate.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+from dj_static import Cling
+application = Cling(application)
